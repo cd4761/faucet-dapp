3fee4146784c7d812239741d150d7259ea9a7bc6
Geonwoo Shin <dn2757@gmail.com>
2019-05-26 02:40:52 +0900

    Update faucet server URL

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ export default {
 
       const self = this;
       // A browser sends an OPTIONS request before a POST request, essentially to check with the server that it's allowed to send the POST request.
-      axios.post('http://localhost:3000/transactions/peth', {
+      axios.post('http://54.159.124.43:3000/transactions/peth', {
         to: account
       })
       .then(function (response) {
@@ -76,7 +76,7 @@ export default {
       
       const self = this;
       // A browser sends an OPTIONS request before a POST request, essentially to check with the server that it's allowed to send the POST request.
-      axios.post('http://localhost:3000/transactions/pdai', {
+      axios.post('http://54.159.124.43:3000/transactions/pdai', {
         to: account
       })
       .then(function (response) {
