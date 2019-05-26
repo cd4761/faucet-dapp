1d8a7e4af7cd7de0517522ece9aae9323e662ba5
Geonwoo Shin <dn2757@gmail.com>
2019-05-26 12:53:16 +0900

    Delete unnecessary files and fix path

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
@@ -10,15 +9,10 @@ var transactionsRouter = require('./routes/transactions');
 var app = express();
 app.use(cors());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/transactions', transactionsRouter);
 
