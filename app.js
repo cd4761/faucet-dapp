a7d1ce30281d892d61d69d048516634cea09cefa
4000D <dkdkajej@gmail.com>
2019-05-26 15:46:47 +0900

    express: serve public directory

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
 
 var indexRouter = require('./routes/index.js');
 
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', express.static(__dirname + '/dist'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 
