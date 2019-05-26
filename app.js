0175d706e9a475bdaafb4e73d429956da125c80a
4000D <dkdkajej@gmail.com>
2019-05-26 13:02:33 +0900

    serve vue as static in express app

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/', express.static(__dirname + '/dist'));
 
 app.use('/transactions', transactionsRouter);
 
